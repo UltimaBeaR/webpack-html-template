6f1b87a736377742c34015c953d8231063717ea9
ultimabear <thebear@yandex.ru>
2022-07-03 23:24:58 +0300

    Initial

---
+++ readme.md
@@ -0,0 +1,7 @@
+Basic webpack setup for HTML/CSS development. Supports scss and live reloading
+
+USAGE
+    Run ```npm install```
+    Then run ```npm start``` every time live reloading should start (opens at http://localhost:5500/)
+
+Inside scss absolute urls (example - url('/img/test.png')) would resolve to public directory (example - projectRoot/public/img/test.png)
---
+++ webpack.config.js
@@ -0,0 +1,77 @@
+const path = require('path');
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    mode: 'development',
+    devtool: 'source-map',
+    stats: 'errors-only',
+    entry: {
+        index: [
+            './js/index.js',
+            './scss/index.scss'
+        ]
+    },
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'bundle.js',
+        clean: true,
+    },
+    module: {
+        rules: [
+            // CSS, PostCSS, Sass
+            {
+                test: /\.(scss|css)$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: "css-loader",
+                        options: {
+                            sourceMap: true,
+                        },
+                    },
+                    {
+                        loader: "postcss-loader",
+                        options: {
+                            sourceMap: true,
+                            postcssOptions: {
+                                plugins: [
+                                    [
+                                        "postcss-preset-env",
+                                        {
+                                            browsers: 'last 2 versions'
+                                        },
+                                    ],
+                                ],
+                            },
+                        },
+                    },
+                    {
+                        loader: 'resolve-url-loader',
+                        options: {
+                            sourceMap: true,
+                            root: path.resolve(__dirname, './public')
+                        }
+                    },
+                    {
+                        loader: "sass-loader",
+                        options: {
+                            sourceMap: true,
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'public'),
+        },
+        compress: true,
+        port: 5500,
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new MiniCssExtractPlugin({ filename: 'bundle.css' })
+    ]
+};
