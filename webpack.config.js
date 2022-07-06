b6c8d6321479cf5702fd2030269856fd6470a411
ultimabear <thebear@yandex.ru>
2022-07-06 21:09:13 +0300

    Небольшой фикс

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
     mode: 'development',
     devtool: 'source-map',
-    stats: 'errors-only',
+    stats: 'minimal',
     entry: {
         index: [
             './js/index.js',
